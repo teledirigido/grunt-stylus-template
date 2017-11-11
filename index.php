cc557ca870faf556624b3eaaf4eb9a6174d6bfaf
miguel <miguel@blacksheepdesign.co.nz>
2017-11-11 12:20:33 -0600

    meta tags added

--- index.php
+++ index.php
@@ -3,12 +3,35 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title></title>
+
+		<?php 
+			$meta = array(
+				'title' => 'PHP, Stylus &amp; Grunt template by miguel.nz',
+				'description' => 'Minimal boilerplate, useful and unobstructive.',
+				'image' => 'og_image.png',
+				'url' => 'http://miguel.nz/template'
+			);
+		?>
+
+		<title><?php echo $meta['title']; ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">        
-		<meta name="description" content="">
-		<meta name="author" content="">
+		<meta name="description" content="<?php echo $meta['description']; ?>">
+		<meta name="author" content="Miguel Garrido">
+		
+		<meta property="og:title" content="<?php echo $meta['title']; ?>">
+		<meta property="og:type" content="website"> 
+		<meta property="og:url" content="<?php echo $meta['url']; ?>">
+		<meta property="og:image" content="<?php echo $meta['image']; ?>">
+		<meta property="og:description" content="<?php echo $meta['description']; ?>">
+
+		<meta name="twitter:card" content="<?php echo $meta['image']; ?>">
+		<meta name="twitter:title" content="<?php echo $meta['title'];  ?>">
+		<meta name="twitter:description" content="<?php echo $meta['description']; ?>">
+		<meta name="twitter:image:src" content="<?php echo $meta['image']; ?>">
+
 		<link rel="stylesheet" href="style.css">
 		<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXX-X"></script> -->
+
 	</head>
 	<body>
 		<script>
@@ -27,7 +50,7 @@
 		<div class="bgblack">
 			<div class="content-wrapper white">
 				<h1>PHP, Stylus &amp; Grunt template</h1>
-				<h2>Minimal boilerplate you must scale</h2>
+				<h2>Minimal boilerplate you must escalate</h2>
 				<h3>jQuery free, Babel JS included for fast front-end development.</h3>
 				<h4>Don't ask if it is responsive, just don't. This is something you should know already.</h4>
 				<h5>OOCSS, simple, unbloated and minimal. <strong>161Kb CSS File size</strong> incluing Font-awesome 4.7.0</h5>
