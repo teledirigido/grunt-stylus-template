02759f8443f70796ed969a9176aa85399b719e15
miguel <miguel@blacksheepdesign.co.nz>
2018-10-12 21:03:36 +0200

    header, footer split and stylus styles added

--- index.php
+++ index.php
@@ -1,51 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-		<?php 
-			$meta = array(
-				'title' => 'PHP, Stylus &amp; Grunt template by miguel.nz',
-				'description' => 'Minimal boilerplate, useful and unobstructive.',
-				'image' => 'og_image.png',
-				'url' => 'http://miguel.nz/template'
-			);
-		?>
-
-		<title><?php echo $meta['title']; ?></title>
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">        
-		<meta name="description" content="<?php echo $meta['description']; ?>">
-		<meta name="author" content="Miguel Garrido">
-		
-		<meta property="og:title" content="<?php echo $meta['title']; ?>">
-		<meta property="og:type" content="website"> 
-		<meta property="og:url" content="<?php echo $meta['url']; ?>">
-		<meta property="og:image" content="<?php echo $meta['image']; ?>">
-		<meta property="og:description" content="<?php echo $meta['description']; ?>">
-
-		<meta name="twitter:card" content="<?php echo $meta['image']; ?>">
-		<meta name="twitter:title" content="<?php echo $meta['title'];  ?>">
-		<meta name="twitter:description" content="<?php echo $meta['description']; ?>">
-		<meta name="twitter:image:src" content="<?php echo $meta['image']; ?>">
-
-		<link rel="stylesheet" href="style.css">
-		<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXX-X"></script> -->
-
-	</head>
-	<body>
-		<script>
-			if (window.location.host==="domain.com" || window.location.host==="www.domain.com") {
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-XXXXXXX-X');
-      }
-		</script>
-
-		<!--[if lte IE 9]>
-			<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
-		<![endif]-->
+<?php include 'header.php'; ?>
 
 		<div class="bgblack">
 			<div class="content-wrapper white">
@@ -120,7 +73,7 @@
 				<div class="mt2">
 					<h2>Flex Grid for modern websites</h2>
 					<h4 class="mb1">At: <pre>stylus/components/grid.styl</pre></h4>
-					<div class="flex-2 pb2">
+					<div class="flex-2 flex-gap-2 pb2">
 						<div class="item">
 							<p>Yes, for modern browsers you can use Flex. Hope you're not into developing IE9 compatible websites.</p>
 						</div>
@@ -142,34 +95,4 @@
 			</p>
 		</div>
 
-		<script>
-      (function(d) {
-        var wf = d.createElement('script'), s = d.scripts[0];
-        wf.src = site.template + '/js/head.js';
-        // wf.async = true;
-        s.parentNode.insertBefore(wf, s);
-        wf.onload = function(){
-          
-          // All JS
-          head.load([
-            site.template + '/fonts/fonts.css',
-            site.template + '/js/polyfill.min.js',
-            site.template + '/js/scripts.min.js',
-            ], function(){
-            	// When files are loaded, load these ones
-              // head.load( site.template + '/js/myfile.js' )
-          })
-          
-          /* 
-          Optional load JS conditional
-          if(site.post_name === 'about'){
-            head.load(
-              site.template + '/js/box-2column-parallax.min.js'
-            )
-          }
-          */
-        }
-      })(document);
-		</script>
-	</body>
-</html>
+<?php include 'footer.php';
